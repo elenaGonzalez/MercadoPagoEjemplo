249498ec68972071bb7ef994b918bce8e38088d6
Ele <elebeatrizgonzalez@gmail.com>
2022-02-18 21:06:39 -0300

    cambio en archivo db.js

--- api/src/db.js
+++ api/src/db.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME,
 } = process.env;
 
 const DATABASE_URL = process.env.DATABASE_URL
@@ -13,7 +13,14 @@ const DATABASE_URL = process.env.DATABASE_URL
 
 const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  native: false,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl:{
+      require: true,
+      rejectUnauthorized: false
+    }
+  } // lets Sequelize know we can use pg-native for ~30% more speed
 });
 
 const basename = path.basename(__filename);
